01c515838c898b8055217a95ba9da9acf95f739a
Dongyoung <wasd507@naver.com>
2022-07-08 01:18:05 +0900

    javascript 0707
    object, function 기능사용하기

---
+++ app.js
@@ -0,0 +1,20 @@
+const calculator = {
+  add: function(a, b){
+    console.log(a + b)
+  },
+  minus: function(a , b){
+    console.log(a - b)
+  },
+  multi: function(a, b){
+    console.log(a * b)
+  },
+  division: function(a, b){
+    console.log(a / b)
+  },
+
+};
+
+calculator.add(5,1);
+calculator.multi(5,1);
+calculator.minus(5,1);
+calculator.division(5,1);
